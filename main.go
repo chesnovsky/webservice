253ca8d6811956a234fd90d2859169fa7a491354
Станислав Чесновский <chesnovsky@MacBook-Pro-Ksenia.local>
2020-03-06 10:22:38 +0300

    [+] controllers

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
-	"github.com/chesnovsky.ru/webservice/models"
+	"github.com/chesnovsky.ru/webservice/controllers"
 )
 
 func main() {
-	u := models.User{
-		ID:        2,
-		FirstName: "Hui",
-		LastName:  "Pizda",
-	}
-	fmt.Println(u)
+	controllers.RegisterControllers()
+	http.ListenAndServe(":3000", nil)
 }
